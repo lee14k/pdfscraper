67edd96654a4e114908c8bda9e1a3861e5214dae
Kailee Hamre <113624108+lee14k@users.noreply.github.com>
2024-01-22 13:19:17 -0500

    fixing deprecated functions from pdf library

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
 import PyPDF2
 
-with open('example.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    num_pages=reader.numPages
+with open('example.PDF', 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    num_pages=len(reader.pages)
     for page_num in range(num_pages):
-        page=reader.getPage(page_num)
-        text=page.extractText()
+        page=reader.pages[page_num]
+        text=page.extract_text()
         print(f"Content of Page{page_num+1}:\n{text}\n")
