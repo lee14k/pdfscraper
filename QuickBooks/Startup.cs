8d12c11de40eb8a8408a8644e56073f52caf74dd
lee14k <kmkovach@mtu.edu>
2024-01-25 18:13:58 -0500

    trying to fix post issues

--- QuickBooks/Startup.cs
+++ QuickBooks/Startup.cs
@@ -4,11 +4,28 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 public class Startup
 {
-    public void ConfigureServices(IServiceCollection services)
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+    
+    // Extract QuickBooks configurations
+    var clientId = Configuration["QuickBooks:ClientId"];
+    var clientSecret = Configuration["QuickBooks:ClientSecret"];
+
+    // Register QuickBooksService with HttpClient and configurations
+    services.AddHttpClient<QuickBooksService>(client =>
     {
-        services.AddControllers();
-        services.AddHttpClient<QuickBooksService>();
-    }
+        // Optionally configure HttpClient here if needed
+    })
+    .ConfigureHttpClient((serviceProvider, httpClient) =>
+    {
+        // Get clientId and clientSecret from your configuration
+        var quickBooksService = serviceProvider.GetRequiredService<IConfiguration>();
+        var clientId = quickBooksService["QuickBooks:ClientId"];
+        var clientSecret = quickBooksService["QuickBooks:ClientSecret"];
+        return new QuickBooksService(httpClient, clientId, clientSecret);
+    });
+}
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
